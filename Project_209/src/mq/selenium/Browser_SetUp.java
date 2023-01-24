5f4b046182dff18ab20d2d0ca90c6d2670843cd8
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-01-24 09:58:09 +0530

    Add files via upload

--- Project_209/src/mq/selenium/Browser_SetUp.java
+++ Project_209/src/mq/selenium/Browser_SetUp.java
@@ -1,22 +1,39 @@
 package mq.selenium;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Browser_SetUp {
+public class Browser_SetUp 
+{
 
 	public static void main(String[] args) 
 	{
+		
 			//launching chrome instance and storing reference into WebDriver Interface
 			WebDriver driver=new ChromeDriver();
 			//Loading New page to Browser
 			driver.get("https://facebook.com");
 			//maximize browser window
-			driver.manage().window().maximize();
+			driver.manage().window().maximize();     //[Optional]
 			
-			//Customer Browser window
-			driver.manage().window().setSize(new Dimension(400, 500));
+			
+			/*
+			 * WebDriver:-->
+			 * 		WebDriver is a interaface class it can manipulate
+			 * 		all browser instances supported by selenium
+			 * 
+			 * get:-->
+			 * 		Get is a method Under webdriver class. It wil load
+			 * 		New page to browser window
+			 * 
+			 * 		Notes:--> We must start site address with valid protocal
+			 * 					http://   or https://
+			 * 
+			 * 			
+			 */
+			
+			
+			
 	}
 
 }
---
+++ WebdriverManager_Project/src/browserlaunch/WebDriverManager_5.java
@@ -0,0 +1,29 @@
+package browserlaunch;
+
+
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManager_5 
+{
+
+	public static void main(String[] args)
+	{
+		
+		WebDriver driver=WebDriverManager.chromedriver().avoidShutdownHook().create();
+		driver.get("http://facebook.com");
+		
+		/*
+		 * avoidShutdownHook:-->
+		 * 			Method keep browser open instead on closing it
+		 * Create:-->
+		 * 			It opens browser 
+		 * get:-->
+		 * 		   It load webpage to browser window
+		 */
+		
+		
+
+	}
+
+}
